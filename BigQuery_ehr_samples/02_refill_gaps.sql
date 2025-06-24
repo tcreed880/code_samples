7ca69726e892010357127685e675f441691f5eaf
tcreed880 <48034133+tcreed880@users.noreply.github.com>
2025-06-24 13:20:46 -0500

    Update 02_refill_gaps.sql
    improve commenting and formatting

--- BigQuery_ehr_samples/02_refill_gaps.sql
+++ BigQuery_ehr_samples/02_refill_gaps.sql
@@ -4,7 +4,9 @@ WITH atorvastatin_exposure AS (
    drug_exposure_start_date,
    days_supply,
    drug_concept_id,
+   --   assign increasing number to each prescription per person, ordered by start date, to track multiple prescriptions
    ROW_NUMBER() OVER (PARTITION BY person_id ORDER BY drug_exposure_start_date) AS rx_order,
+   -- look at previous prescription start date and supply days
    LAG(drug_exposure_start_date) OVER (PARTITION BY person_id ORDER BY drug_exposure_start_date) AS previous_start,
    LAG(days_supply) OVER (PARTITION BY person_id ORDER BY drug_exposure_start_date) AS previous_days_supply
  FROM
@@ -21,6 +23,7 @@ SELECT
  drug_exposure_start_date,
  previous_start,
  previous_days_supply,
+ --  find number of days between when the patient should have run out of their previous supply and when they actually filled the next prescription, filling in 0 for null
  DATE_DIFF(drug_exposure_start_date, DATE_ADD(previous_start, INTERVAL IFNULL(previous_days_supply, 0) DAY), DAY) AS refill_gap_days
 FROM
  atorvastatin_exposure
@@ -30,11 +33,13 @@ ORDER BY
  person_id, drug_exposure_start_date;
 
 
-QUery for late_refiller flag > 20% 
+-- QUery for late_refiller flag > 20% 
 SELECT
  person_id,
  COUNT(*) AS total_refills,
  SUM(CASE WHEN refill_gap_days > 30 THEN 1 ELSE 0 END) AS late_refills,
+ -- safe divide prevents division-by-zero errors
+ -- create flag for late_refills, calculate rate of late refills vs on-time, flag those with 20% + rates
  SAFE_DIVIDE(SUM(CASE WHEN refill_gap_days > 30 THEN 1 ELSE 0 END), COUNT(*)) AS late_refill_rate,
  CASE
    WHEN SAFE_DIVIDE(SUM(CASE WHEN refill_gap_days > 30 THEN 1 ELSE 0 END), COUNT(*)) > 0.2 THEN 1
